e6292a5d1cf470471d09c8454af7a91419bca2f6
Victor Kilander <victor@nasa.se>
2019-08-13 16:10:47 +0200

    Updates

---
+++ precache-manifest.64e54af1732f6bdc74556a3d67689ce2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e2028adbe7fd90eb2aa15743d2d069",
+    "url": "/the-burger-list/index.html"
+  },
+  {
+    "revision": "914014f104c282f699b6",
+    "url": "/the-burger-list/static/css/main.bb284bf0.chunk.css"
+  },
+  {
+    "revision": "085c5f7fcd891f25fe4b",
+    "url": "/the-burger-list/static/js/2.a4e7c35b.chunk.js"
+  },
+  {
+    "revision": "914014f104c282f699b6",
+    "url": "/the-burger-list/static/js/main.0d3118d9.chunk.js"
+  },
+  {
+    "revision": "f21a4e55d416d9e556bb",
+    "url": "/the-burger-list/static/js/runtime~main.3edb5433.js"
+  },
+  {
+    "revision": "1bb66f782eef699ae78687c5ad00768a",
+    "url": "/the-burger-list/static/media/GT-Pressura-Mono-Bold.1bb66f78.ttf"
+  },
+  {
+    "revision": "c7f41f860339111eafbd2b6426de120d",
+    "url": "/the-burger-list/static/media/GT-Pressura-Mono-Light.c7f41f86.ttf"
+  },
+  {
+    "revision": "b57fdd065fc080701e1f3f364abfc2c3",
+    "url": "/the-burger-list/static/media/GT-Pressura-Mono-Regular.b57fdd06.ttf"
+  },
+  {
+    "revision": "7d68792e38ed9f91249a926412c254a8",
+    "url": "/the-burger-list/static/media/beer.7d68792e.gif"
+  },
+  {
+    "revision": "8b78603f728e8e91eadea68de511a2e4",
+    "url": "/the-burger-list/static/media/burger.8b78603f.gif"
+  },
+  {
+    "revision": "8c9e38909a371ec785380b2b1ba5db0c",
+    "url": "/the-burger-list/static/media/burger_asmr.8c9e3890.mp3"
+  },
+  {
+    "revision": "5ee866bf2ec25fc566717a72a91e7b85",
+    "url": "/the-burger-list/static/media/chair.5ee866bf.svg"
+  },
+  {
+    "revision": "760db2da7ccf2dcd5e7c1dc2375b403d",
+    "url": "/the-burger-list/static/media/foodtruck.760db2da.svg"
+  },
+  {
+    "revision": "9be8b1d7af9fed7816adbabd84175449",
+    "url": "/the-burger-list/static/media/fries.9be8b1d7.gif"
+  },
+  {
+    "revision": "10a8fec9ad150406ee18f92cb2441eaf",
+    "url": "/the-burger-list/static/media/headphones-2.10a8fec9.gif"
+  },
+  {
+    "revision": "199c4a00a2e2729c81390db0131ab0b8",
+    "url": "/the-burger-list/static/media/logo.199c4a00.png"
+  },
+  {
+    "revision": "aba701fa43739d67d67e3a22f98052d5",
+    "url": "/the-burger-list/static/media/noa.aba701fa.png"
+  },
+  {
+    "revision": "156a480678d7966d072891bb7e1e5870",
+    "url": "/the-burger-list/static/media/takeaway.156a4806.svg"
+  }
+]);
