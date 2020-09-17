d2a2ba13489832b6565f2ec5c839f927b4c64894
vctrklndr <victor@nasa.se>
2020-09-17 17:10:57 +0200

    Updating fetch url

--- src/Functions/FetchBurgerJoints.js
+++ src/Functions/FetchBurgerJoints.js
@@ -1,5 +1,5 @@
 async function fetchBurgerJoints () {
-  const response = await fetch('https://api.myjson.com/bins/d9awj');
+  const response = await fetch('https://tulegatan.nu/burgers.json');
   const burgerjoints = await response.json();
   return burgerjoints;
 }
